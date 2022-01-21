2547cd3560a57488cce5cfdabdfd7fc4c546f307
theadambyrne <adamrbyrne@gmail.com>
2022-01-21 20:58:48 +0000

    websec

--- next.config.js
+++ next.config.js
@@ -1,9 +1,30 @@
 /** @type {import('next').NextConfig} */
+const { createSecureHeaders } = require("next-secure-headers");
+
 const nextConfig = {
 	reactStrictMode: true,
 	i18n: {
 		locales: ["en"],
 		defaultLocale: "en",
+	},
+	async headers() {
+		return [
+			{
+				source: "/(.*)",
+				headers: createSecureHeaders({
+					contentSecurityPolicy: {
+						directives: {
+							defaultSrc: ["'self'", "https://adambxrne.vercel.app"],
+							styleSrc: ["'self'", "'unsafe-inline'"],
+							imgSrc: ["'self'"],
+							baseUri: "self",
+							formAction: "self",
+							frameAncestors: true,
+						},
+					},
+				}),
+			},
+		];
 	},
 };
 
