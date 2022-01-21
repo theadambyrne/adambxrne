dbebf66dd831a3de67c0c85499876aa719fd6083
theadambyrne <adamrbyrne@gmail.com>
2022-01-21 21:02:15 +0000

    undo

--- next.config.js
+++ next.config.js
@@ -1,30 +1,10 @@
 /** @type {import('next').NextConfig} */
-const { createSecureHeaders } = require("next-secure-headers");
 
 const nextConfig = {
 	reactStrictMode: true,
 	i18n: {
 		locales: ["en"],
 		defaultLocale: "en",
-	},
-	async headers() {
-		return [
-			{
-				source: "/(.*)",
-				headers: createSecureHeaders({
-					contentSecurityPolicy: {
-						directives: {
-							defaultSrc: ["'self'", "https://adambxrne.vercel.app"],
-							styleSrc: ["'self'", "'unsafe-inline'"],
-							imgSrc: ["'self'"],
-							baseUri: "self",
-							formAction: "self",
-							frameAncestors: true,
-						},
-					},
-				}),
-			},
-		];
 	},
 };
 
